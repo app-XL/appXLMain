680d0be6288c7f358df32511213845798bac086a
Sankar Vema <sans.vema@gmail.com>
2018-01-03 20:11:37 +0530

    Update and rename server.js to sys.js

---
+++ sys.js
@@ -0,0 +1,9 @@
+console.log('muApps CodeGenSys Initiated...');
+var tmpl=require('tmpl/tmplMaster');
+
+var codegen=require('sys/codegen');
+console.log('muApps CodeGenSys running...');
+codegen.run(tmpl);
+
+
+
